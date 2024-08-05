d56e3bd761a3630f295c3e79944ed0a725c99966
rohit mahajan <rohitam35@gmail.com>
2024-08-05 19:19:23 +0530

    aws file added

--- csv_file_analysis_project/settings.py
+++ csv_file_analysis_project/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',      
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
