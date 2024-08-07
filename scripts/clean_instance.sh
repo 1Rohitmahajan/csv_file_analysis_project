50a1314dab8e7f8dc4e554073b3ddab4705b797e
rohit mahajan <rohitam35@gmail.com>
2024-08-07 17:37:41 +0530

    add foders

---
+++ scripts/clean_instance.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+#
+sudo rm -rf /home/ubuntu/csv_file_analysis_project/*
---
+++ scripts/python_dependencies.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+virtualenv /home/ubuntu/env
+source /home/ubuntu/env/bin/activate
+pip install -r /home/ubuntu/csv_file_analysis_project/requirements.txt
